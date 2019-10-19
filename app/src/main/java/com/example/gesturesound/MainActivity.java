7fd56cadf2ad2df5793a1ed51c75cac494863ae9
Karthik Ramesh Iyer <Karthik.iyer2@yandex.com>
2019-10-20 04:51:51 +0530

    add sound

--- app/src/main/java/com/example/gesturesound/MainActivity.java
+++ app/src/main/java/com/example/gesturesound/MainActivity.java
@@ -4,8 +4,13 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
@@ -13,16 +18,36 @@ import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
     public static float SENSOR_SAMPLING_PERIOD = 10F;
+    int snareCt = 0;
+    int hangCt = 0;
     private LineGraphSeries<DataPoint> lineGraphSeries_forZ;
     private SensorManager sensorManager;
     private float currX;
     private long newTime, startTime, prevTime;
-
+    private boolean playingSnare = false, playingHanging = false, playingCymbals = false;
+    private TextView xAcc, zAcc;
+    private MediaPlayer snare, tomtom, cym;
+    private Button cymbals;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         currX = 0;
+        snare = MediaPlayer.create(this, R.raw.snare);
+        tomtom = MediaPlayer.create(this, R.raw.tomtom);
+        cym = MediaPlayer.create(this, R.raw.cymbals);
+
+        cymbals = findViewById(R.id.cymbals);
+
+        cymbals.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playCymbals();
+            }
+        });
+
+        xAcc = findViewById(R.id.textView1);
+        zAcc = findViewById(R.id.textView2);
 
         startTime = newTime = prevTime = System.currentTimeMillis();
 
@@ -48,11 +73,34 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onSensorChanged(SensorEvent event) {
+
         newTime = System.currentTimeMillis();
         if (newTime - prevTime >= SENSOR_SAMPLING_PERIOD) {
             if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-                if (Math.abs(event.values[2]) > 0.5) {
-                    currX = event.values[2];
+                float xAcceleration = Math.round(event.values[0]);
+                float zAcceleration = Math.round(event.values[2]);
+                if (Math.abs(xAcceleration) > 1) {
+                    currX = xAcceleration;
+                    xAcc.setText("x = " + xAcceleration);
+                    zAcc.setText("z = " + zAcceleration);
+
+                    if (playingSnare && xAcceleration > -10 && zAcceleration < -5) {
+                        playingSnare = !playingSnare;
+                    } else if (playingHanging && zAcceleration > -5 && xAcceleration < -10) {
+                        playingHanging = !playingHanging;
+                    } else if (xAcceleration > 0 || zAcceleration > 0) {
+                    }
+//                    else if(xAcceleration < -12 && zAcceleration < -7){
+//                        Log.d("TAG: ","CYMBALS x: "+xAcceleration+" z: "+zAcceleration);
+//                    }
+                    else if (xAcceleration < -10 && zAcceleration > -5) {
+//                        Log.d("TAG: ","SNARE x: "+xAcceleration+" z: "+zAcceleration);
+                        playSnare();
+                    } else if (zAcceleration < -5 && xAcceleration > -10) {
+//                        Log.d("TAG: ","HANGING x: "+xAcceleration+" z: "+zAcceleration);
+                        playHanging();
+                    }
+
                 } else {
                     currX = 0f;
                 }
@@ -63,6 +111,33 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     }
 
+    private void playSnare() {
+        if (!playingSnare) {
+            playingSnare = !playingSnare;
+            snare.start();
+            Log.d("TAG: ", "PLAYING SNARE" + (++snareCt));
+        }
+    }
+
+
+    private void playHanging() {
+        if (!playingHanging) {
+            playingHanging = !playingHanging;
+            tomtom.start();
+            Log.d("TAG: ", "PLAYING HANGING" + (++hangCt));
+        }
+    }
+
+    private void playCymbals() {
+        if (!playingCymbals) {
+            playingCymbals = !playingCymbals;
+            cym.start();
+            Log.d("TAG: ", "PLAYING HANGING" + (++hangCt));
+        }
+    }
+
+//    private void playBassDrum(){}
+
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
@@ -72,7 +147,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onResume() {
         super.onResume();
 
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_FASTEST);
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_GAME);
+//        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD), SensorManager.SENSOR_DELAY_FASTEST);
     }
 
     @Override
